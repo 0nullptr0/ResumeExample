e37882d39ed17b7c9d0c970beb97e54374c56a86
0Nullptr0 <JakubNowakVS@gmail.com>
2022-03-15 11:31:06 -0400

    Game starter camera instancing

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class GameStarter : MonoBehaviour
 {
     public GameObject prefab_;
+    public GameObject camera_prefab;
     void Start()
     {
         Instantiate(prefab_, new Vector3(MapVariables.x*3, 0, MapVariables.z*3), Quaternion.identity);
+        Instantiate(camera_prefab, new Vector3(MapVariables.x*3, 1, MapVariables.z*3), Quaternion.identity);
     }
 }
